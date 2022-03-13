62077a46120677948db319e2ec17c679f6c8deac
SATO Yusuke <yusuke.sato.zz@gmail.com>
2022-03-14 01:55:26 +0900

    Add testcase for type annotation

--- src/java16/RecordClass.java
+++ src/java16/RecordClass.java
@@ -6,20 +6,9 @@ import java.io.PrintStream;
 
 public class RecordClass {
     public static void main(String[] args) throws FileNotFoundException {
+        Java16RecordClass r = new Java16RecordClass();
         System.setOut(new PrintStream(new File("nul")));
-
-        Record1 r1 = new Record1(1, 2);
-        System.out.println(r1);
-        Record1 r2 = new Record1(3, 4);
-        System.out.println(r2);
+        r.writeRecord();
     }
 }
 
-record Record1(int x, int y) {}
-
-record Record2(int x, int y) {
-    public Record2(int x, int y) {
-        this.x = x * 2;
-        this.y = y * 2;
-    }
-}
